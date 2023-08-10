13d8fb34c8566cdb8cfeead9892386e1d48e090c
alireza salehi <aliunity751@gmail.com>
2023-08-10 08:46:17 +0330

    add responder

---
+++ src/Res.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pickmap\Responder;
+
+class Res
+{
+    public static function success(string|object|array|null $data = null,$statusCode = 200) : object
+    {
+        return self::response( "success" , null , $data , $statusCode);
+    }
+    
+    public static function fail(string|object|array|null $data = null,$statusCode = 200) : object
+    {
+        return self::response( "fail" , null , $data , $statusCode);
+    }
+
+    public static function error(string $message ,string|object|array|null $data = null,$statusCode = 500): object
+    {
+        return self::response( "error" , $message , $data , $statusCode);
+    }
+
+    public static function response($status,$message,$data,$code)
+    {
+        return Response()->json([
+            "status"        => $status,
+            "message"       => $message,
+            "data"          => $data
+        ],$code);
+    }
+}
